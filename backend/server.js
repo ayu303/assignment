d10af1bfe784e4bc95ed31a6f2c173084d32f9a0
ayu303 <131518566+ayu303@users.noreply.github.com>
2024-01-08 21:38:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,32 @@
-require('dotenv').config();
-
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv'); // Load environmental variables
-dotenv.config();
+const bodyParser = require('body-parser');
+
+const Datastore = require('nedb');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/service-provider-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const db = new Datastore();
+app.get('/', (req, res) => {
+  res.send('Hello, this is the backend server!');
+});
+app.post('/api/submit-form', (req, res) => {
+  const { fullName, contactNo, email, selectedService, selectedTeam } = req.body;
+
+  db.insert({ fullName, contactNo, email, selectedService, selectedTeam }, (err, newDoc) => {
+    if (err) {
+      console.error('Error saving form data to database:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      console.log('Form data saved to database:', newDoc);
+      res.json(newDoc);
+    }
+  });
 });
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-// Main routes
-app.use('/api/services', require('./routes/services'));
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
